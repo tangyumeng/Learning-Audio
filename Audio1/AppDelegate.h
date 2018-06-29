5e93b5ad2611b54810bdd813cb3033dbc2082db3
tangyumeng <tangyumeng@gmail.com>
2018-06-29 22:08:28 +0800

    Initial Commit

---
+++ Audio1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Audio1
+//
+//  Created by yumeng tang on 2018/6/29.
+//  Copyright © 2018年 yumeng tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Audio1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Audio1
+//
+//  Created by yumeng tang on 2018/6/29.
+//  Copyright © 2018年 yumeng tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
